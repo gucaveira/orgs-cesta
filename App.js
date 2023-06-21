56c02751c7e470e5c766d8ed19f31e1e08cdcead
gustavo rocha <pereiragustavo215@gmail.com>
2023-06-21 20:09:26 -0300

    organizando os componentes

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { StatusBar, SafeAreaView } from 'react-native';
-
-import Cesta from './src/telas/Cesta';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
+import Cesta from './src/telas/cesta';
+import mock from './src/mocks/cesta';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if (!fonteCarregada) {
+    return <AppLoading />
+  }
+
   return (
     <SafeAreaView>
-      <StatusBar/>
-        <Cesta/>
+      <StatusBar />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
